5b89368f2eb479943d103735204c1a37bb309402
amy <amy@amy-pc>
2021-04-14 15:24:29 +0900

    #210414 amy Add 다이크스트라알고리즘

--- amy.pratice/src/amy/practice/Algorithm/School/Greedy/NondirectionalDijkstra.java
+++ amy.pratice/src/amy/practice/Algorithm/School/Greedy/NondirectionalDijkstra.java
@@ -7,9 +7,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import amy.practice.Algorithm.School.Greedy.NondirectionalDijkstra.DEdge;
-
-public class DirectionalDijkstra {
+public class NondirectionalDijkstra {
 	class DEdge {
 		String vertex;
 		int weight;
@@ -27,23 +25,29 @@ public class DirectionalDijkstra {
 		HashMap<String, DEdge> dist = new HashMap<String, DEdge>();
 		IntStream.range(0, vertexes.length).forEach(i -> dist.put(vertexes[i], new DEdge(null, Integer.MAX_VALUE)));
 		HashMap<String, LinkedList<DEdge>> edgeMap = new HashMap<>();
+		DEdge de = null;
 		for (String[] edge : edges) {
-			String ori = edge[0];
-			String dest = edge[1];
+			String v1 = edge[0];
+			String v2 = edge[1];
 			int w = Integer.parseInt(edge[2]);
-			DEdge de = new DEdge(dest, w);
-			if (!edgeMap.containsKey(ori)) {
-				edgeMap.put(ori, new LinkedList<DEdge>());
+			if (!edgeMap.containsKey(v1)) {
+				edgeMap.put(v1, new LinkedList<DEdge>());
 			}
-			edgeMap.get(ori).add(de);
+			if (!edgeMap.containsKey(v2)) {
+				edgeMap.put(v2, new LinkedList<DEdge>());
+			}
+			de = new DEdge(v2, w);
+			edgeMap.get(v1).add(de);
+			de = new DEdge(v1, w);
+			edgeMap.get(v2).add(de);
 		}
 
-		//시작 정점을 미방문정점으로부터 제거
+		// 시작 정점을 미방문정점으로부터 제거
 		unvisited.remove(unvisited.indexOf(start));
-		//시작 정점은 가중칙0 초기화
+		// 시작 정점은 가중칙0 초기화
 		dist.put(start, new DEdge(start, 0));
-		
-		//현재 정점 초기화
+
+		// 현재 정점 초기화
 		String depart = start;
 
 		while (true) {
@@ -58,23 +62,27 @@ public class DirectionalDijkstra {
 			// 인접연결된 모든 정점 갱신
 			while (list != null && !list.isEmpty()) {
 				DEdge dE = list.poll();
-				int edgeW = amt + dE.weight;
-				// 만약 기존의 해당정점의 가중치보다 현재 경로의 가중치가 더 작다면
-				if (dist.get(dE.vertex).weight >= edgeW) {
-					// 가중치를 갱신
-					dist.get(dE.vertex).weight = edgeW;
-					// 선행정점을 현재 시작정점으로 갱신
-					dist.get(dE.vertex).vertex = depart;
-				}
-				// 아직 방문하지 않은 정점중 갱신된 가중치가 인접정정중 최소값이라면 다음 인접정점은
-				if ( dist.get(dE.vertex).weight < min) {
-					nP = dE.vertex;
-					min = dist.get(dE.vertex).weight;
+				if (unvisited.contains(dE.vertex)) {
+
+					int edgeW = amt + dE.weight;
+					// 만약 기존의 해당정점의 가중치보다 현재 경로의 가중치가 더 작다면
+					if (dist.get(dE.vertex).weight >= edgeW) {
+						// 가중치를 갱신
+						dist.get(dE.vertex).weight = edgeW;
+						// 선행정점을 현재 시작정점으로 갱신
+						dist.get(dE.vertex).vertex = depart;
+					}
+					// 아직 방문하지 않은 정점중 갱신된 가중치가 인접정정중 최소값이라면 다음 인접정점은
+					if (dist.get(dE.vertex).weight < min) {
+						nP = dE.vertex;
+						min = dist.get(dE.vertex).weight;
+					}
 				}
 			}
 			// 비방문 정점이 없다면 처리 종료
-			if(unvisited.size() <= 0)  break;
-			//인접정접 중 미방문 정점이 없을 경우, 가중치가 적은 정점으로 재계산
+			if (unvisited.size() <= 0)
+				break;
+			// 인접정접 중 미방문 정점이 없을 경우, 가중치가 적은 정점으로 재계산
 			if (nP == null) {
 				int minW = Integer.MAX_VALUE;
 				for (String uP : unvisited) {
@@ -83,7 +91,7 @@ public class DirectionalDijkstra {
 					}
 				}
 			}
-			//중간과정 출력 및 다음 재계산 정점 갱신
+			// 중간과정 출력 및 다음 재계산 정점 갱신
 			System.out.printf("출발정점 = %s", depart);
 			depart = nP;
 			System.out.printf(" 도착정점 = %s 선행정점 %s 가중치 %d\r\n", depart, dist.get(depart).vertex,
@@ -92,7 +100,7 @@ public class DirectionalDijkstra {
 		}
 		// 결과 출력
 		System.out.printf("출발점 %s ~  도착첨 %s 까지의 최단거리 %d   ", start, end, dist.get(end).weight);
-		//a~f까지의 최단경로출력
+		// a~f까지의 최단경로출력
 		StringBuilder path = new StringBuilder();
 		String v = end;
 		path.append(v);
@@ -101,7 +109,7 @@ public class DirectionalDijkstra {
 			path.append(v);
 		}
 		System.out.println(" 경로" + path.toString());
-		//각정점의 최소가중치 및 선행정점 출력
+		// 각정점의 최소가중치 및 선행정점 출력
 		for (int i = vertexes.length - 1; i > 0; i--) {
 			String key = vertexes[i];
 			System.out.printf("정점 %s 선생정점 %s 최소가중치 %d \r\n", key, dist.get(key).vertex, dist.get(key).weight);
@@ -110,14 +118,14 @@ public class DirectionalDijkstra {
 
 	public static void main(String[] args) {
 
-		NondirectionalDijkstra dd = new NondirectionalDijkstra();
+		DirectionalDijkstra dd = new DirectionalDijkstra();
 		String[][] graphs = null;
 		String start = "a";
 		String end = "f";
-		graphs = new String[][] { { "a", "b", "1" }, { "a", "c", "7" }, { "a", "d", "4" }, { "a", "e", "6" },
-				{ "b", "d", "2" }, { "c", "f", "3" }, { "d", "c", "3" }, { "d", "e", "4" }, { "e", "c", "2" },
-				{ "e", "d", "2" }, { "e", "f", "2" } };
-		int expected = 8;
+		graphs = new String[][] { { "a", "b", "1" }, { "a", "c", "6" }, { "b", "c", "2" }, { "b", "d", "5" },
+				{ "b", "e", "4" }, { "c", "d", "2" }, { "c", "e", "3" }, { "d", "e", "1" }, { "d", "f", "4" },
+				{ "e", "d", "2" }, { "e", "f", "7" } };
+		int expected = 9;
 		System.out.println("expected : " + expected);
 		String[] vertexes = new String[] { "a", "b", "c", "d", "e", "f" };
 		dd.dijkstra(vertexes, graphs, start, end);
