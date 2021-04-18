72e59dce4cbbf9e7e0f2dc4700aa961ac3323422
amy <amykim4229@gmail.com>
2021-04-18 14:17:04 +0900

    #210418 amy Modify 불필요코드 삭제

--- amy.pratice/src/amy/practice/Algorithm/School/Greedy/PrimAlgorithm.java
+++ amy.pratice/src/amy/practice/Algorithm/School/Greedy/PrimAlgorithm.java
@@ -1,7 +1,6 @@
 package amy.practice.Algorithm.School.Greedy;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.PriorityQueue;
