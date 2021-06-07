be42dbcab2e4f2d7a5c31bbf31a903e36761236e
amy <amykim4229@gmail.com>
2021-06-07 14:27:06 +0900

    210607 Amy FIx 출력내용 변경

--- amy.pratice/src/amy/practice/Algorithm/School/Sort/HeapSortTest.java
+++ amy.pratice/src/amy/practice/Algorithm/School/Sort/HeapSortTest.java
@@ -8,7 +8,7 @@ public class HeapSortTest extends AbstractSort {
 	@Override
 	protected int[] sortProcess(int[] array) {
 		int[] heap = new MyHeap(array).getHeapArray();
-		System.out.println("1 : " + Arrays.toString(heap));
+		System.out.println("최초힙 : " + Arrays.toString(heap));
 		int unsortedNode = heap.length - 1;
 		while (unsortedNode > 0) {
 			swap(heap, 0, unsortedNode);
@@ -34,7 +34,8 @@ public class HeapSortTest extends AbstractSort {
 				parent--;
 				continue;
 			}
-			int childMax = heap[child1] >= (child2 > node  ?  0 : heap[child2]) ?  child1 : child2;
+			cnt++;
+			int childMax = heap[child1] >= (child2 > node ? 0 : heap[child2]) ? child1 : child2;
 			if (heap[parent] < heap[childMax]) {
 				swap(heap, parent, childMax);
 				swapDown(heap, childMax);
@@ -45,15 +46,16 @@ public class HeapSortTest extends AbstractSort {
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// int[] array = { 50, 10, 20, 40, 30 };
+		int[] array = { 50, 10, 20, 40, 30 };
 		HeapSortTest s = new HeapSortTest();
 		s.sortName = "힙정렬";
-		// s.sort(array);
+		s.sort(array);
 		int[] array2 = { 10, 20, 30, 40, 50 };
 		s.sort(array2);
 		int[] array3 = { 50, 40, 30, 20, 10 };
 		s.sort(array3);
+		int[] array4 = { 80, 60, 70, 40, 20,30,50,10 };
+		s.sort(array4);
 
 	}
 
